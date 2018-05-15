75e5895cfa098adacf5347b0e6ebafb4550255ac
Ellie Keen <elliekeen95@gmail.com>
2018-05-15 17:54:10 +0100

    Add land plane method

--- spec/airport-spec.js
+++ spec/airport-spec.js
@@ -3,12 +3,25 @@ describe('Airport', function() {
   var airport;
   beforeEach(function() {
     airport = new Airport();
-  })
+  });
 
 describe('when creating the airport', function() {
   it('should have a hangar area for planes', function() {
     expect(airport.hangar).toEqual([]);
-  })
+  });
+
+  it('should have a capacity attribute', function() {
+    expect(airport.capacity).toEqual(10);
+  });
+});
+
+describe('landing a plane', function() {
+  var plane = jasmine.createSpyObj('plane', ['takeOff']);
+  it('should allow a plane to land', function() {
+    airport.land(plane);
+    expect(airport.hangar).toContain(plane);
+  });
+
 });
 
 });
--- src/airport.js
+++ src/airport.js
@@ -1,3 +1,10 @@
 var Airport = function() {
+  const CAPACITY = 10;
   this.hangar = [];
+  this.capacity = CAPACITY;
+
 };
+
+Airport.prototype.land = function(plane) {
+  this.hangar.push(plane);
+}
