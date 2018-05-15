0c911036ba3e9724ff43568dae69771027a60a25
Mark Channer <markchanner@gmail.com>
2018-05-15 19:05:03 +0100

    Adds takeOff method

--- spec/airport-spec.js
+++ spec/airport-spec.js
@@ -1,27 +1,36 @@
 describe('Airport', function() {
-
+  var weather;
   var airport;
+
   beforeEach(function() {
-    airport = new Airport();
+    weather = 'sunny';
+    airport = new Airport(weather);
   });
 
-describe('when creating the airport', function() {
-  it('should have a hangar area for planes', function() {
-    expect(airport.hangar).toEqual([]);
+  describe('when creating the airport', function() {
+    it('should have a hangar area for planes', function() {
+      expect(airport.hangar).toEqual([]);
+    });
+
+    it('should have a capacity attribute', function() {
+      expect(airport.capacity).toEqual(10);
+    });
   });
 
-  it('should have a capacity attribute', function() {
-    expect(airport.capacity).toEqual(10);
-  });
-});
-
-describe('landing a plane', function() {
-  var plane = jasmine.createSpyObj('plane', ['takeOff']);
-  it('should allow a plane to land', function() {
-    airport.land(plane);
-    expect(airport.hangar).toContain(plane);
+  describe('landing a plane', function() {
+    var plane = jasmine.createSpyObj('plane', ['tempMethod']);
+    it('should allow a plane to land', function() {
+      airport.land(plane);
+      expect(airport.hangar).toContain(plane);
+    });
   });
 
-});
-
+  describe('taking off a plane', function() {
+    var plane = jasmine.createSpyObj('plane', ['tempMethod']);
+    it('should allow a plane to take off', function() {
+      airport.land(plane);
+      airport.takeOff(plane);
+      expect(airport.hangar).not.toContain(plane);
+    });
+  });
 });
--- src/airport.js
+++ src/airport.js
@@ -1,10 +1,14 @@
-var Airport = function() {
+var Airport = function(weather) {
   const CAPACITY = 10;
+  this.weather = weather;
   this.hangar = [];
   this.capacity = CAPACITY;
-
 };
 
 Airport.prototype.land = function(plane) {
   this.hangar.push(plane);
-}
+};
+
+Airport.prototype.takeOff = function(plane) {
+  this.hangar.splice(this.hangar.indexOf(plane), 1);
+};
